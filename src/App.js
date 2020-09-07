24d7a46f079f9838cafe5baf55fdfb4cdaea9084
Tero <tero@starupcommons.org>
2020-09-07 06:56:13 +0300

    alpha

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Box, Flex, Text } from "@blend-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Flex justifyContent="center" alignItems="center" minHeight="100vh">
+        <Box width="200px" p={30} border="2px solid" borderRadius={4}>
+          <Text textAlign="center" textStyle="h3">
+            Center this
+          </Text>
+        </Box>
+      </Flex>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { ThemeProvider } from "@blend-ui/core";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const MainApp = () => (
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainApp />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
