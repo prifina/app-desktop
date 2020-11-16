0b353f9212af76359f3b0b703e610a4510df80cd
tro9999 <tero@prifina.com>
2020-11-16 08:02:36 +0200

    removed login story

--- stories/Login.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Login from "../src/pages/Login";
-
-export default { title: "Login" };
-
-export const login = () => <Login />;
-login.story = {
-  name: "Login",
-};
