60e9a882dd3b73c972db042b2c920fd435a59812
tro9999 <tero@prifina.com>
2022-03-01 17:24:44 +0200

    test env fix& shell script example

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -8,7 +8,7 @@ node testing-init-ddb.js
 pushd ../packages/app-testing
 
 export USERNAME=testing-1
-export PASSWORD='Testing12!#'
+export PASSWORD='********'
 export TEST_URL='http://localhost:3000'
 
 # running jest script... 
