e901afa393275b5dc966da25616d70cb61bb77b7
hamza <hamzahalilovic.12@gmail.com>
2022-03-03 21:12:58 +0100

    test run script update

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -9,12 +9,18 @@ node testing-init-ddb.js
 pushd ../packages/app-testing
 
 export USERNAME=testing-1
-export PASSWORD='********'
+export PASSWORD=Testing12!#
+
+export USERNAME_LENGTH=6
+
 
 export TEST_URL='http://localhost:3000'
 
 # running jest script... 
+# node_modules/.bin/jest __tests__/2022-2-11/login.test.js 
+# node_modules/.bin/jest __tests__/2022-2-11/registration.test.js 
 # node_modules/.bin/jest __tests__/2022-2-11/home.test.js 
 node_modules/.bin/jest __tests__/2022-3-4/appMarket.test.js 
+# node_modules/.bin/jest __tests__/2022-3-4/userMenu.test.js 
 
 popd
