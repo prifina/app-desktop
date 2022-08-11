c1f9b8464f1439a542deb2e21cda66eecc59d249
hamza <hamzahalilovic.12@gmail.com>
2022-08-11 20:02:29 +0200

    update

--- packages/desktop-app/src/components/AddWidgetModal.js
+++ packages/desktop-app/src/components/AddWidgetModal.js
@@ -7,7 +7,7 @@ import {
   ModalFooter,
   ModalHeader,
 } from "@blend-ui/modal";
-import { Box, Button, Text, Flex, useTheme } from "@blend-ui/core";
+import { Box, Button, Text, Flex, Image, useTheme } from "@blend-ui/core";
 
 import styled from "styled-components";
 
@@ -25,7 +25,13 @@ import { useHistory } from "react-router-dom";
 
 i18n.init();
 
-const AddWidgetModal = ({ onClose, widgetData, viewID, ...props }) => {
+const AddWidgetModal = ({
+  onClose,
+  widgetData,
+  viewID,
+  widgetConfig,
+  ...props
+}) => {
   const { currentUser } = useAppContext();
   const history = useHistory();
   console.log("NEW APP ", currentUser);
@@ -43,6 +49,7 @@ const AddWidgetModal = ({ onClose, widgetData, viewID, ...props }) => {
   const theme = useTheme();
 
   console.log("widgetData", widgetData);
+  console.log("widgetConfig", widgetConfig);
 
   const [dialogOpen, setDialogOpen] = useState(true);
 
@@ -58,21 +65,59 @@ const AddWidgetModal = ({ onClose, widgetData, viewID, ...props }) => {
     setActiveItem(widgetData[+e.target.value]);
   };
 
-  const [activeViewArray, setActiveViewArray] = useState([]);
+  let existingArray = widgetConfig;
+
+  const [activeViewArray, setActiveViewArray] = useState(existingArray);
 
   let id = viewID;
 
   const handleAddToArray = e => {
     setActiveViewArray(oldArray => [...oldArray, activeItem]);
+    // setActiveViewArray([...activeViewArray, activeItem]);
   };
 
   console.log("active item", activeItem);
   console.log("active array", activeViewArray);
 
   useEffect(() => {
+    console.log("log3");
+
     // storing input name
     localStorage.setItem(`viewsContent-${id}`, JSON.stringify(activeViewArray));
   }, [activeViewArray]);
+
+  props.propDrill(activeViewArray);
+
+  const List = styled("ul")`
+    margin: 0;
+    padding: 8px;
+    font-size: 12px;
+    font-weight: 500;
+    width: 280px;
+    // overflow-y: scroll;
+  `;
+
+  const ListItem = styled.li`
+    // justify-content: space-between;
+    align-items: center;
+    list-style-type: none;
+    padding: 2px 14px 2px 12px;
+    &:active {
+      background: #e7dbf0;
+    }
+    &:hover {
+      background: #e7dbf0;
+    }
+    cursor: pointer;
+    height: 40px;
+    font-size: 14px;
+    border-radius: 8px;
+    display: flex;
+  `;
+
+  const onOptionClicked = value => () => {
+    setActiveItem(value);
+  };
 
   return (
     <React.Fragment>
@@ -88,7 +133,7 @@ const AddWidgetModal = ({ onClose, widgetData, viewID, ...props }) => {
       >
         <ModalContent
           style={{
-            background: colors.baseTertiary,
+            background: "white",
             width: "806px",
             height: "412px",
             borderRadius: 5,
@@ -96,29 +141,56 @@ const AddWidgetModal = ({ onClose, widgetData, viewID, ...props }) => {
           marginLeft="317px"
         >
           <ModalHeader>Find Widgets</ModalHeader>
-          <ModalBody paddingLeft="36px" paddingRight="36px" paddingTop="37px">
+          <ModalBody paddingRight="36px">
             <Flex>
               <Flex
-                display="flex"
                 flexDirection="column"
                 justifyContent="center"
                 alignItems="space-between"
+                style={{ overflowY: "scroll", width: 280, height: "100%" }}
               >
-                <select onChange={handleActiveItem}>
-                  {widgetData.map((item, index) => (
-                    <option key={index} value={index}>
-                      {item.widget.title}
-                    </option>
-                  ))}
-                </select>
+                {widgetData.map((item, index) => (
+                  <List>
+                    <ListItem
+                      key={index}
+                      // value={index}
+                      // onClick={handleActiveItem}
+                      onClick={onOptionClicked(item)}
+                    >
+                      <Image
+                        src={item.widget.icon}
+                        width="21px"
+                        height="21px"
+                      />
+                      <Text ml={8}>{item.widget.title}</Text>
+                    </ListItem>
+                  </List>
+                ))}
               </Flex>
               <Box>
-                {}
-                <Text>{activeItem.widget.title}</Text>
-                <Text>{activeItem.widget.shortDescription}</Text>
-                <Button onClick={handleAddToArray}>Add to</Button>
-                <Button>Learn More</Button>
-                <Button onClick={onClose}>Close</Button>
+                <Flex mb={10}>
+                  <Image
+                    src={activeItem.widget.icon}
+                    width="64px"
+                    height="64px"
+                  />
+                  <Text>{activeItem.widget.title}</Text>
+                </Flex>
+                <Text fontWeight={500}>{activeItem.widget.title}</Text>
+                <Text color="#4D5150" mb={44}>
+                  {activeItem.widget.shortDescription}
+                </Text>
+                <Flex flexDirection="column" justifyContent="center">
+                  <Button mb={12} onClick={handleAddToArray}>
+                    Add to
+                  </Button>
+                  <Button variation="outline" mb={12}>
+                    Learn More
+                  </Button>
+                  <Button variation="outline" onClick={onClose}>
+                    Close
+                  </Button>
+                </Flex>
               </Box>
             </Flex>
           </ModalBody>
