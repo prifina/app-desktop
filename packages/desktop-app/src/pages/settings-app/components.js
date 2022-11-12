c2ac1004073553925f9b6356c87566757c55f26c
hamza <hamzahalilovic.12@gmail.com>
2022-11-12 02:21:07 +0100

    account settings 0.1.0

--- packages/desktop-app/src/pages/settings-app/components.js
+++ packages/desktop-app/src/pages/settings-app/components.js
@@ -4,3 +4,35 @@ import { Flex, Text } from "@blend-ui/core";
 import { ReactComponent as PrifinaLogoImage } from "../../assets/settings-app/settings-logo.svg";
 
 import PropTypes from "prop-types";
+
+import styled from "styled-components";
+
+export const UnorderedList = styled.ul`
+  /* */
+  list-style-type: none;
+  margin: 0;
+  list-style-position: outside;
+  padding-inline-start: 20px;
+  margin-block-start: 0px;
+  padding: 0;
+  padding-left: 20px;
+`;
+
+export const ListItem = styled.li`
+  /* */
+  ::before {
+    content: "•";
+    color: ${props =>
+      props.verified
+        ? props.theme.colors.baseSecondary
+        : props.theme.colors.baseMuted}; // from theme
+    display: inline-block;
+    width: 0.9em;
+    margin-left: -0.9em;
+    font-size: 2em;
+  }
+  span {
+    position: relative;
+    top: -5px;
+  }
+`;
